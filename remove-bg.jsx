927148a0bad266c7a914a34b17cfc747ab632a41
Kavindu Pasan Kavithilaka <kavithipublications@gmail.com>
2020-12-13 23:14:58 +0530

    Update remove-bg.jsx

--- remove-bg.jsx
+++ remove-bg.jsx
@@ -1,6 +1,7 @@
-﻿/*------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------
 This scripts is created by Kavindu Pasan Kavithilaka
 on 2020-03-15
+updatedon 2020-12-13
 This scripts supports Photoshop CC 2020 and above versions.
 How to use the script: https://youtu.be/6ICVsi2pWyk
 --------------------------------------------------------------------------------------*/
@@ -20,7 +21,19 @@ colorRef.rgb.green = 255;
 colorRef.rgb.blue = 255;
 //Set blow to true to make the background transparent.
 var isTransparent = true;
+//Set below to true to use an image as background
+var isImageBg = true;
+//If isImageBg is set to true,
+//it's required to the background image to be preopened in photohsop
+//Backdound image must be the active document
 //-----------------------------------------------------------------------------------
+
+
+//Check if it's selected to use an image as background
+	if(isImageBg){
+		//Store background image and a variable
+		var doc_bg = app.activeDocument;
+	}
 
 
 //Cheks if the source folder is null
@@ -28,7 +41,7 @@ var isTransparent = true;
   {
 	//The following line will list all files (not only image files) on the source folder.
 	//If you have any non-image files (even hidden) , please see the next comment.
-    	var fileList = sourceFolder.getFiles();
+    var fileList = sourceFolder.getFiles();
 	//Comment the above line and uncomment the following line to filter specific file types.
 	//Try filter files types if the script fails.
 	//var fileList = sourceFolder.getFiles(/\.(jpg|png|tif|psd|crw|cr2|nef|dcr|dc2|raw|heic)$/i);
@@ -66,6 +79,20 @@ for(var a = 0 ;a < fileList.length; a++){
 	else{
 		app.activeDocument.selection.fill(colorRef);
 	}
+	
+	
+	//Check if it's selected to use an image as background
+	if(isImageBg){
+		//Store main document to a variable
+		var main_doc = app.activeDocument;
+		//Swich to background image
+		app.activeDocument = doc_bg;
+		//Copy background to the main image
+		app.activeDocument.activeLayer.duplicate(main_doc, ElementPlacement.PLACEATEND);
+		//Switch to the main image
+		app.activeDocument = main_doc;
+	}
+	
 
 	//Now the image is proccessed. Next step is saving the image.
 	//Create the file name
